ee4b1d57a70e5f98066a20683e9fe4e89761eab7
Chandana <dasarichandana111@gmail.com>
2022-02-16 09:27:56 +0530

    File_Handling

--- handling_file/file_handling/csv_file.py
+++ handling_file/file_handling/csv_file.py
@@ -34,13 +34,44 @@ path = r"C:\Users\Jagadeesh\PycharmProjects\Chandu1432\directory_file\files_dire
 #         d[row[2]] += [row[0]]
 #     print(d)
 
-path = r"C:\Users\Jagadeesh\PycharmProjects\Chandu1432\directory_file\files_directory\csv_files\test.csv"
-with open(path) as file:
-    sum = 0
-    r_obj = csv.reader(file)
-    header = next(r_obj)
-    print(header)
-    for row in r_obj:
-        sum += int(row[1])
-    print(sum)
+# path = r"C:\Users\Jagadeesh\PycharmProjects\Chandu1432\directory_file\files_directory\csv_files\test.csv"
+# with open(path) as file:
+#     sum = 0
+#     r_obj = csv.reader(file)
+#     header = next(r_obj)
+#     print(header)
+#     for row in r_obj:
+#         sum += int(row[1])
+#     print(sum)
+
+
+path = r"C:\Users\Jagadeesh\PycharmProjects\Chandu1432\directory_file\files_directory\csv_files\vaccination_data.csv"
+# with open(path) as file:
+#     r_obj = csv.reader(file)
+#     header = next(r_obj)
+#     for row in r_obj:
+#         #print(row)
+#         print(row[5], end=' ')
+
+# from collections import defaultdict
+# with open(path) as file:
+#     r_obj = csv.reader(file)
+#     d = defaultdict(list)
+#     header = next(r_obj)
+#     for row in r_obj:
+#         d[row[0]] += [row[5]]
+#     print(d)
+
+# from collections import defaultdict
+# with open(path) as file:
+#     r_obj = csv.DictReader(file)
+#     d = defaultdict(list)
+#     next(r_obj)
+#     for row in r_obj:
+#         if row["DATE_UPDATED"]:
+#             d[row["DATE_UPDATED"]] += [(row['TOTAL_VACCINATIONS'],row['PERSONS_VACCINATED_1PLUS_DOSE'],row['COUNTRY'])]
+#     print(d)
+# res = sorted(d.items())
+#print(res[-1])
+
 
